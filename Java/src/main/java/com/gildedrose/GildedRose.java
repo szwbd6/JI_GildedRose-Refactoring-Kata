51ae9700dadccabd131bb9cba874ca7ac852d03b
szwbd6 <jaganathan.ilango@gmail.com>
2023-06-05 09:21:34 +0200

    Refactored to a Wrapped Class and Subclass based on category

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import com.gildedrose.model.InventoryItem;
+
 class GildedRose {
     Item[] items;
 
@@ -9,72 +11,7 @@ class GildedRose {
 
     public void updateInventory() {
         for (Item item : items) {
-            updateItem(item);
-        }
-    }
-
-    private  void updateItem(Item item) {
-        updateQuality(item);
-        updateExpiry(item);
-
-        if (isExpired(item)) {
-            handleExpired(item);
-        }
-    }
-    private void updateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            increaseQuality(item);
-
-            if (item.sellIn < 11) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                increaseQuality(item);
-            }
-
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                return;
-            }
-        else {
-            decreaseQuality(item);
-        }
-    }
-
-    private void updateExpiry(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        item.sellIn--;
-    }
-
-    private void handleExpired(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = 0;
-        }
-        else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        else {
-            decreaseQuality(item);
-        }
-
-    }
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
-        }
-    }
-    private void decreaseQuality(Item item) {
-        if  (item.quality > 0){
-            item.quality--;
+            InventoryItem.createInstance(item).updateItem();
         }
     }
 }
---
+++ Java/src/main/java/com/gildedrose/model/InventoryItem.java
@@ -0,0 +1,66 @@
+package com.gildedrose.model;
+
+import com.gildedrose.Item;
+
+public class InventoryItem {
+
+    public static final String CONJURED = "Conjured";
+    protected Item item;
+
+    public static InventoryItem createInstance(Item item) {
+
+        switch(item.name)
+        {
+            case Legendary.NAME:
+                return new Legendary(item);
+            case Cheese.NAME:
+                return new Cheese(item);
+            case BackstagePass.NAME:
+                return new BackstagePass(item);
+            default:
+                return new InventoryItem(item);
+
+        }
+    }
+
+    protected InventoryItem(Item item) {
+        this.item = item;
+    }
+
+    public void updateItem() {
+        updateQuality();
+        updateExpiry();
+
+        if (isExpired()) {
+            handleExpired();
+        }
+    }
+
+    protected void updateQuality() {
+        decreaseQuality();
+    }
+
+    protected void updateExpiry() {
+        item.sellIn--;
+    }
+
+    protected void handleExpired() {
+        decreaseQuality();
+    }
+
+    protected boolean isExpired() {
+        return item.sellIn < 0;
+    }
+
+    protected void increaseQuality() {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    protected void decreaseQuality() {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+}
